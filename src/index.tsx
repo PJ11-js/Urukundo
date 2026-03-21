bcd31bf0595513b5971584ef251957534519078e
PJ11-js <pierrengendakuriyo5@gmail.com>
2026-03-22 00:44:39 +0100

    Block mobile back navigation causing white screen

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Bloquer la navigation arrière sur mobile
+history.pushState(null, '', window.location.href);
+window.addEventListener('popstate', () => {
+  history.pushState(null, '', window.location.href);
+});
+
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Could not find root element to mount to');
+if (!rootElement) throw new Error('Could not find root element');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
