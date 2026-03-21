c411230b3ccfebd55675758cf99ef39758891572
PJ11-js <pierrengendakuriyo5@gmail.com>
2026-03-21 20:44:31 +0100

    Fix page blanche Chrome - disable SW cache

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,8 @@
-const CACHE_NAME = 'urukundo-v2';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
+// Service Worker désactivé temporairement
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-// Ne pas cacher — toujours fetch depuis le réseau
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
 });
